34a70afbb57f09215ef0909473fe432982ff6a36
Gary Josack <gary@byoteki.com>
2022-12-05 22:15:02 -0800

    Update UpdateURL

--- LiveSplit.Spelunky/Factory.cs
+++ LiveSplit.Spelunky/Factory.cs
@@ -15,9 +15,9 @@ namespace LiveSplit.Spelunky
 
     public string UpdateName => this.ComponentName;
 
-    public string XMLURL => "";
-
-    public string UpdateURL => "";
+    public string UpdateURL => "https://raw.githubusercontent.com/spelunky-fyi/LiveSplit-Spelunky-HD/main/";
+    
+    public string XMLURL => UpdateURL + "Components/update.LiveSplit.Spelunky.xml";
 
     public Version Version => Version.Parse("2.0.0");
 
